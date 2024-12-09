de952485017c5c96604bb0e04c641fce79a6ff8d
Rafael Dal Forno <rafa_fln15@hotmail.com>
2024-12-09 17:13:52 -0300

    adicionando as imagens no projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
   return  gulp.src('./src/styles/*.scss')
@@ -7,7 +8,19 @@ function styles(){
             .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images(){
+  return  gulp.src('./src/images/**/*')
+            .pipe(imagemin({
+              optimizationLevel: 5,
+              progressive: true,
+              interlaced: true,
+              svgoPlugins: [{ removeViewBox: false }]
+            }))
+            .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
   gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
